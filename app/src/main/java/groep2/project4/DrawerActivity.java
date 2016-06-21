c058b4b32215e8f321b8151b8bdc3e11da1f006a
Dominic <drvh15@gmail.com>
2016-06-21 15:03:28 +0200

    Update on Fragments

--- app/src/main/java/groep2/project4/DrawerActivity.java
+++ app/src/main/java/groep2/project4/DrawerActivity.java
@@ -1,8 +1,10 @@
 package groep2.project4;
 
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.app.Fragment;
 
 public class DrawerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -79,16 +82,21 @@ public class DrawerActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Fragment fragment;
 
         if (id == R.id.fietstrommels) {
+            fragment = new FietsTrommels();
             // wat er gebeurt als je op fietstrommels drukt, etc.
-        } else if (id == R.id.diefstallen) {
+        } else if (id == R.id.diefstallen) { fragment = new Diefstallen();
 
-        } else if (id == R.id.locatie) {
+        } else if (id == R.id.locatie) { fragment = new Locatie();
 
-        } else if (id == R.id.route) {
+        } else if (id == R.id.route) { fragment = new Route();
 
-        }
+        } else {fragment = new FietsTrommels();}
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.drawer_start, fragment).commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
