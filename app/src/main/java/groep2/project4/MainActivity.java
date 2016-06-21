a78e8800a8d658c2ca03b0ec69ef0da5a1c3a76a
leverage010 <dlukasprive@gmail.com>
2016-06-21 13:11:56 +0200

    Drawer added

--- app/src/main/java/groep2/project4/MainActivity.java
+++ app/src/main/java/groep2/project4/MainActivity.java
@@ -1,13 +1,28 @@
 package groep2.project4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startknop = (Button) findViewById(R.id.startknop);
+        final Intent toDrawer = new Intent(this, DrawerActivity.class);
+
+        startknop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(toDrawer);
+            }
+        });
     }
+
+
 }
